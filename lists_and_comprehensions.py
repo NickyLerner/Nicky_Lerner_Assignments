f77256f89f04c34c46f7359777f2ac347f9b2046
Nicky <llerner@fwparker.org>
2019-02-07 13:56:55 -0600

    test workflow

--- lists_and_comprehensions.py
+++ lists_and_comprehensions.py
@@ -1,7 +1,7 @@
 #LISTS (29PTS TOTAL)
 #In these exercises you should should use functions as needed.  All functions should have comments to describe their purpose.
 
-
+dfgh
 
 # PROBLEM 1 (Using List Comprehensions - 6pts)
 # Use the list comprehension method to do the following:
