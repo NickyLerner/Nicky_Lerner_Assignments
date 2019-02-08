252e07d841b9751840c9c07f546ebc74b260c0f8
Nicky <llerner@fwparker.org>
2019-02-07 22:34:00 -0600

    this is completed

--- lists_and_comprehensions.py
+++ lists_and_comprehensions.py
@@ -1,7 +1,9 @@
-#LISTS (29PTS TOTAL)
-#In these exercises you should should use functions as needed.  All functions should have comments to describe their purpose.
+import random
 
-dfgh
+# LISTS (29PTS TOTAL)
+# In these exercises you should should use functions as needed.  All functions should have comments to describe their purpose.
+
+
 
 # PROBLEM 1 (Using List Comprehensions - 6pts)
 # Use the list comprehension method to do the following:
@@ -9,6 +11,18 @@ dfgh
 # b) Make a list of even numbers from 20 to 40
 # c) Make a list of squares from 1 to 100 (1 ** 2 to 100 ** 2)
 
+list_prob_1_1 = []
+list_prob_1_2 = []
+list_prob_1_3 = []
+
+for i in range(100):
+    list_prob_1_1.append(i+1)
+    if i % 2 == 0 and i < 21:
+        list_prob_1_2.append(i+20)
+    list_prob_1_3.append((i+1)**2)
+
+print(list_prob_1_2)
+print()
 
 #PROBLEM 2 (8-ball - 5pts)
 # A magic 8-ball, when asked a question, provides a random answer from a list.
@@ -18,18 +32,40 @@ answer_list = [ "It is certain", "It is decidedly so", "Without a \
 doubt", "Yes, definitely", "You may rely on it", "As I see it, \
 yes", "Most likely", "Outlook good", "Yes", "Signs point to yes",
 "Reply hazy try again", "Ask again later", "Better not tell you \
-now", "Cannot predict now", "Concentrate and ask again", "Don ' t \
+now", "Cannot predict now", "Concentrate and ask again", "Don't \
 count on it", "My reply is no", "My sources say no", "Outlook \
 not so good", "Very doubtful" ]
 
+question = input("What is your question?  ")
+if "?" in question:
+    print(answer_list[random.randrange(len(answer_list))])
 
+print()
 # PROBLEM 3 (Shuffle - 8pts)
 # A playing card consists of a suit (Heart, Diamond, Club, Spade) and a value (2,3,4,5,6,7,8,9,10,J,Q,K,A).
 # Create a list of all possible playing cards, which is a deck.
 # Then create a function that shuffles the deck, producing a random order. Print the random deck. 
 # Then deal yourself a hand of 5 cards off the top.  Print the hand.  Print the remaining deck.
 
+suit_list = ["Heart", "Diamond", "Club", "Spade"]
+value_list = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+deck = []
+hand = []
 
+for i in range(len(suit_list)):
+    for j in range(len(value_list)):
+        deck.append([suit_list[i], value_list[j]])
+
+random.shuffle(deck)
+print(deck)
+
+for i in range(5):
+    a = deck.pop(0)
+    hand.append(a)
+
+print(hand)
+print(deck)
+print()
 # PROBLEM 4 (Illinois Pick 4 - 10pts)
 # Lotteries are known to give awful odds of winning, and incredibly low expected returns on your invevestment.
 # You will buy 10000 Illinois Pick 4 tickets in a simulation.
@@ -38,3 +74,33 @@ not so good", "Very doubtful" ]
 # After drawing the official numbers, you will go back through your list and check to see how many wins you got.
 # The numbers must be an exact match in the exact position.
 # Each ticket is $1.  If you win, you get $5000.  Simulate spending $10,000 on Pick 4 tickets, and see your return.
+
+ticket_list = []
+official_numbers = [random.randrange(10), random.randrange(10), random.randrange(10), random.randrange(10)]
+
+for i in range(10000):
+    ticket_numbers = []
+    a = random.randrange(10)
+    b = random.randrange(10)
+    c = random.randrange(10)
+    d = random.randrange(10)
+    ticket_numbers.append(a)
+    ticket_numbers.append(b)
+    ticket_numbers.append(c)
+    ticket_numbers.append(d)
+    ticket_list.append(ticket_numbers)
+
+winners = ticket_list.count(official_numbers)
+
+# winners = 3
+
+gains = (-10000 + (winners*5000))
+
+# gains = 2
+
+if gains >= 0:
+    print("You have won", end=" ")
+else:
+    print("You have lost", end=" ")
+print(gains, end=" ")
+print("dollars.")
