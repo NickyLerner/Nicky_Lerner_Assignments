e1a3d6a4e3ab72d7bafed40452ca6daff827a879
Nicky <llerner@fwparker.org>
2019-02-14 14:25:12 -0600

    this is completed for real this time

--- challenge_problems.py
+++ challenge_problems.py
@@ -1,9 +1,27 @@
+import random
+
 # PROBLEM 1 (Fibonacci)
 ## The Fibonacci sequence is a sequence of numbers that starts with 1, followed by 1 again.
 # Every next number is the sum of the two previous numbers.
 # I.e., the sequence starts with 1, 1, 2, 3, 5, 8, 13, 21,...
 # Write a program that calculates and prints the Fibonacci sequence
 # until the numbers get higher than 1000.
+
+done = False
+a = int(1)
+
+b = int(1)
+
+
+while not done:
+    print(a, end=" ")
+    print(b, end=" ")
+    a += int(b)
+    b += int(a)
+    if a > 1000 or b > 1000:
+        done = True
+
+print()
 
 # PROBLEM 2 (Dice Sequence)
 # You roll five six-sided dice, one by one.
@@ -13,9 +31,20 @@
 # but “1, 1, 4, 3, 6” is not. Determine the
 # probability of success using a simulation of a large number of trials.
 
+
+
 # PROBLEM 3 (Number Puzzler)
 # A, B, C, and D are all different digits.
 # The number DCBA is equal to 4 times the number ABCD.
 # What are the digits?
 # Note: to make ABCD and DCBA conventional numbers, neither A nor D can be zero.
 # Use a quadruple-nested loop to solve. 
+
+for i in range(1, 10):
+    for j in range(10):
+        for k in range(10):
+            for l in range(1, 10):
+                number_1 = str(i) + str(j) + str(k) + str(l)
+                number_2 = str(l) + str(k) + str(j) + str(i)
+                if (int(number_2)/int(number_1)) == 4:
+                    print(number_1)
