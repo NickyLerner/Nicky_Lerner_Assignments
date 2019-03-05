2a005d60344d52096a55e99e61aa64c2cce8834c
Nicky <llerner@fwparker.org>
2019-03-05 11:40:14 -0600

    this is not completed

--- Searching/ch15Lab.py
+++ Searching/ch15Lab.py
@@ -16,16 +16,59 @@ with open('dictionary.txt', 'r') as f:
 with open('AliceInWonderland.txt') as f:
     alice_full_text_by_line = [x.strip() for x in f]
 
+with open('AliceInWonderland200.txt') as f:
+    alice_chapter_one_by_line = [x.strip() for x in f]
+
 print("---LINEAR SEARCH---")
 
-alice_full_text_by_words = []
 
-'''for line in range(len(alice_full_text_by_line)):
-    # print(line, alice_full_text_by_line[line])
-    alice_current_line_by_words = split_line(alice_full_text_by_line[line])
-    for words in alice_current_line_by_words:
-        if words.upper() not in dictionary:
-            print("In line", str(line + 1) + ", possible word,", words + ", misspelled.")'''
+def linear_search(text):
+    i = 0
+    good_word = True
+    for line in range(len(text)):
+        text_current_line_by_words = split_line(text[line])
+        for words in text_current_line_by_words:
+            while i < len(dictionary) and words.upper() != dictionary[i]:
+                i += 1
+            if i == len(dictionary):
+                good_word = False
+            if not good_word:
+                print("In line", str(line + 1) + ", possible word,", words + ", misspelled.")
+            good_word = True
+            i = 0
+
+
+# linear_search(alice_chapter_one_by_line)
 
 print("---BINARY SEARCH---")
 
+
+def binary_search(text):
+    good_word = True
+    for line in range(len(text)):
+        text_current_line_by_words = split_line(text[line])
+        for words in text_current_line_by_words:
+            lower_boundary = 0
+            upper_boundary = len(dictionary) - 1
+            while lower_boundary <= upper_boundary and good_word:
+                middle_pos = (lower_boundary + upper_boundary) // 2
+                # Figure out if we:
+                # move up the lower bound, or
+                # move down the upper bound, or
+                # we found what we are looking for
+                if dictionary[middle_pos] < words:
+                    lower_boundary = middle_pos + 1
+                elif dictionary[middle_pos] > words:
+                    upper_boundary = middle_pos - 1
+                else:
+                    good_word = False
+            if not good_word:
+                print("In line", str(line + 1) + ", possible word,", words + ", misspelled.")
+            good_word = True
+
+
+binary_search(alice_chapter_one_by_line)
+
+'''# Loop until we find the item, or our upper/lower bounds meet
+for words in alice_full_text_by_line
+    '''
