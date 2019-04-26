757dc02e63357e2ddbccc658e19b280040494a16
Nicky <llerner@fwparker.org>
2019-04-26 09:08:37 -0500

    this is completed

--- Gravity_Calculator/main.py
+++ Gravity_Calculator/main.py
@@ -5,7 +5,7 @@ from kivy.core.window import Window
 G = 6.67e-11
 
 
-Window.size = (300, 400)
+Window.size = (600, 400)
 
 
 class GravityApp(App):
@@ -15,10 +15,24 @@ class GravityApp(App):
 
 class GravityLayout(BoxLayout):
     def gravitize(self, obj1, obj2, distance):
-        equation = "(" + str(obj1) + "*" + str(obj2) + "*" + str(G) + ")" + "/" + str(float(distance)**2)
-        eval(equation)
+        try:
+            self.display.font_size = 40
+            equation = "(" + str(obj1) + "*" + str(obj2) + "*" + str(G) + ")" + "/" + str(float(distance)**2)
+            answer = eval(equation)
+            self.display.text = str(answer)
+        except ZeroDivisionError:
+            self.display.text = "You Cannot Have Two Objects in the Same Place"
+            self.display.font_size = 30
+        except OverflowError:
+            self.display.font_size = 30
+            self.display.text = "That's too much stuff"
+        except:
+            self.display.font_size = 30
+            self.display.text = "Please Enter a Value for all Options"
 
 
 if __name__ == "__main__":
     app = GravityApp()
     app.run()
+
+# for some reason the background color was not changing
