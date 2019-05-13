998f92412cfd4db12f2794f0ee1ff9cdcfc45edf
Nicky <llerner@fwparker.org>
2019-05-13 10:46:03 -0500

    this is completed

--- kivyFormatter/main.py
+++ kivyFormatter/main.py
@@ -13,14 +13,17 @@ class FormattingApp(App):
 class FormattingLayout(BoxLayout):
     def change_text(self, words):
         self.display.text = words
+
+
     def light_switch(self, boolean):
         if not boolean:
             self.display.color = 1, 1, 1, 1
         else:
             self.display.color = 0, 0, 0, 0
-    def color_change(self, value):
-        self.r = value
-        Window.clearcolor = self.r, self.g, self.b, 1
+
+
+    def color_change(self, r, g, b):
+        Window.clearcolor = r/255, g/255, b/255, 1
 
 if __name__ == "__main__":
     app = FormattingApp()
